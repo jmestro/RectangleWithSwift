aa6e1ca84ed21366b232f3627d8057282a884f91
Joe Mestrovich <joe@mizmovac.com>
2022-04-13 10:01:43 -0700

    Initial Commit

---
+++ RectangleWithSwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RectangleWithSwift
+//
+//  Created by Joe Mestrovich on 4/13/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
